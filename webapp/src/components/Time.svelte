286b8b88fa10d51c8b01f8dea2b99f908f463f31
Jason Schwarzenberger <jason@credisense.io>
2020-12-16 13:54:53 +1300

    fix time when paging.

--- webapp/src/components/Time.svelte
+++ webapp/src/components/Time.svelte
@@ -2,10 +2,10 @@
   import fromUnixTime from "date-fns/fromUnixTime";
   import formatDistanceToNow from "date-fns/formatDistanceToNow";
   export let date;
-  let d = fromUnixTime(date);
-  let datetime = d.toISOString();
-  let title = d.toLocaleString();
-  let dateString = formatDistanceToNow(d, { addSuffix: true });
+  $: d = fromUnixTime(date);
+  $: datetime = d.toISOString();
+  $: title = d.toLocaleString();
+  $: dateString = formatDistanceToNow(d, { addSuffix: true });
 </script>
 
 <time {datetime} {title}>{dateString}</time>
